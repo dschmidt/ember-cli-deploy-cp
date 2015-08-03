861f3f226dc0201b792d4e5e86d53dc121bc7f81
Dominik Schmidt <dev@dominik-schmidt.de>
2015-08-03 16:26:05 +0200

    Add initial implementation

--- index.js
+++ index.js
@@ -1,6 +1,75 @@
 /* jshint node: true */
 'use strict';
 
+
+var Promise   = require('ember-cli/lib/ext/promise');
+var DeployPluginBase = require('ember-cli-deploy-plugin');
+var cpr = require('cpr');
+var SilentError         = require('silent-error');
+
 module.exports = {
-  name: 'ember-cli-deploy-cp'
+  name: 'ember-cli-deploy-cp',
+
+  createDeployPlugin: function(options) {
+    var DeployPlugin = DeployPluginBase.extend({
+      name: options.name,
+      defaultConfig: {
+        distDir: function(context) {
+          return context.distDir;
+        },
+        deleteFirst: false,
+        overwrite: false,
+        confirm: true,
+        filter: undefined,
+
+        didDeployMessage: function(context){
+          if (context.revisionKey) {
+            return "Copied revision " + context.revisionKey + ".";
+          }
+        },
+      },
+      requiredConfig: ['destDir'],
+      configure: function(/* context */) {
+        this.log('validating config');
+
+        ['distDir', 'deleteFirst', 'overwrite', 'confirm', 'didDeployMessage', 'filter'].forEach(this.applyDefaultConfigProperty.bind(this));
+
+        this.log('config ok');
+      },
+
+      upload: function(/* context */) {
+        var distDir = this.readConfig('distDir');
+        var destDir = this.readConfig('destDir');
+
+        var options = {
+          deleteFirst: this.readConfig('deleteFirst'),
+          overwrite: this.readConfig('overwrite'),
+          confirm: this.readConfig('confirm')
+        };
+
+        if(this.readConfig('filter') !== undefined) {
+          options.filter = this.readConfig('filter');
+        }
+
+        return new Promise(function(resolve, reject) {
+          cpr(distDir, destDir, options, function(err, files) {
+            if(err) {
+              return reject(new SilentError('Could not copy files' + err));
+            }
+
+            resolve();
+          });
+        });
+      },
+      didDeploy: function(/* context */){
+        var didDeployMessage = this.readConfig('didDeployMessage');
+        if (didDeployMessage) {
+          this.log(didDeployMessage);
+        }
+      }
+    });
+    return new DeployPlugin();
+  }
+
+
 };
